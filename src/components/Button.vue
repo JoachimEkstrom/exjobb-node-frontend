278d9b211557e6cf483de559b52d27557e1c7f74
Joachim <joachim.ekstroem@gmail.com>
2021-05-07 15:22:30 +0200

    first commit, basic Vue app.

---
+++ src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="pushed">{{text}}</button>
+        <button @click="readOPC">READ</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String,
+            default: 'My Button'
+        }
+    },
+    methods: {
+        pushed(){
+            fetch('http://localhost:3005/call', {
+                method: 'POST',
+                headers: {"Access-Control-Allow-Origin" : '*', 'Content-Type': 'application/json'},
+                body: JSON.stringify({a: 10.54, b: 30.54}) 
+            })
+            .then(response => response.json())
+            .then(data => console.log(data));
+             
+            
+        },
+        readOPC(){
+            fetch('http://localhost:3005/read', {
+                method: 'GET',
+                headers: {"Access-Control-Allow-Origin" : '*', 'Content-Type': 'application/json'},
+            })
+            .then(response => response.json())
+            .then(data => console.log(data));
+             
+            
+        }
+    }
+    
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <iframe src='http://localhost:3000/d-solo/_Ehr2dfMz/nodejs?orgId=1&refresh=5s&from=1620282173011&to=1620303773012&panelId=2' width="450" height="200" frameborder="0"></iframe>
+    <div id="nav">
+      <router-link to="/">Home</router-link> 
+      
+    </div>
+  </div>
+</template>
+<style scoped>
+#nav a {
+  font-weight: bold;
+  color: #0eeb0e;
+}
+
+</style>
